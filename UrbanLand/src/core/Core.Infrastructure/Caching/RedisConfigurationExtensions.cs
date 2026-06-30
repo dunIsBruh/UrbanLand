71634883f353ffefbcd80a90be66f18b2c66c60b
dunIsBruh <daniargarafiev6@gmail.com>
2026-06-30 12:51:34 +0300

    add GlobalUsing.cs in Core.Infrastructure

--- UrbanLand/src/core/Core.Infrastructure/Caching/RedisConfigurationExtensions.cs
+++ UrbanLand/src/core/Core.Infrastructure/Caching/RedisConfigurationExtensions.cs
@@ -1,6 +1,3 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace Core.Infrastructure.Caching;
 
 public static class RedisConfigurationExtensions
---
+++ UrbanLand/src/core/Core.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,6 @@
+global using System.Reflection;
+global using MassTransit.Mediator;
+global using Microsoft.EntityFrameworkCore;
+global using MassTransit;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
